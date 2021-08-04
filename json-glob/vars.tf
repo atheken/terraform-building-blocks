75bc2acd74468c7b2374dd1696e166e29ea0ca73
Andrew Theken <andrew@wildbit.com>
2021-08-04 14:40:50 -0400

    Move variables to correct file.

---
+++ json-glob/vars.tf
@@ -0,0 +1,15 @@
+variable files {
+  type = list(object({
+    path = string
+    required = bool
+  }))
+  description = "A list of files to load, some can be optional"
+}
+
+variable base_path {
+  description = "The base path from which to load the files"
+}
+
+variable base_object {
+  description = "A starting object into which additional json objects will be merged."
+}
