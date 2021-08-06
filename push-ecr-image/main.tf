9386b08bb6c7441d9610143d0d82dac96fc545ca
Andrew Theken <andrew@wildbit.com>
2021-08-06 10:51:44 -0400

    Pass role args.

--- push-ecr-image/main.tf
+++ push-ecr-image/main.tf
@@ -8,11 +8,19 @@ variable local_image {
 
 variable publisher_role_arn {
   description = "The role that should be used for logging in to the docker ECR and publishing. If not set, the ambient credentials will be used."
-  default = ''
+  default = ""
+}
+
+variable assume_role_session_name {
+  default = "ecr-publish"
 }
 
 data external container_checksum {
   program = ["${abspath(path.module)}/container-checksum.sh", var.local_image ]
+}
+
+locals {
+  assume_role_args = length(var.publisher_role_arn) > 0 ? "${var.publisher_role_arn} ${var.assume_role_session_name}"
 }
 
 resource null_resource image_push {
@@ -20,6 +28,6 @@ resource null_resource image_push {
     image_id = data.external.container_checksum.result.ImageID
   }
   provisioner local-exec {
-    command = "${abspath(path.module)}/push-ecr.sh ${var.local_image} ${var.registry_url} ${var.publisher_role_arn}"
+    command = "${abspath(path.module)}/push-ecr.sh ${var.local_image} ${var.registry_url} ${local.assume_role_args}"
   }
 }
