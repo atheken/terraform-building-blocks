d545ee732d4c552aee5459a049cbbd606eff7ee8
Andrew Theken <andrew@wildbit.com>
2021-08-14 12:04:22 -0400

    Add a repo info lookup module.

---
+++ ecr-repo-info/main.tf
@@ -0,0 +1,12 @@
+
+data aws_ecr_repository repo {
+ name = var.ecr_repo_name
+}
+
+output registry_uri {
+  value = split(data.aws_ecr_repository.repo.repository_url, "/")[0]
+}
+
+output repository {
+  value = data.aws_ecr_repository.repo
+}
---
+++ ecr-repo-info/var.tf
@@ -0,0 +1,3 @@
+variable ecr_repo_name {
+  description = "The name of the repo to look up."
+}
