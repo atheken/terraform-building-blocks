7986d17f2346fe481d26550ebc731abedccfb03d
Andrew Theken <andrew@attr.io>
2022-11-08 08:20:34 -0500

    Remove AWS CLI as a docker push dependency.

--- push-ecr-image/docker-push.sh
+++ push-ecr-image/docker-push.sh
@@ -3,22 +3,8 @@ set -euo pipefail
 
 image=$1
 registry_url=$2
-assume_role_arn=${3:-''}
-assume_role_session_name=${4:-'terraform-push-docker'}
-
+password=$3
 docker tag $image $registry_url/$image
 
-if [[ ! -z $assume_role_arn ]]; then
-
-  temp_role=$(aws sts assume-role \
-       --role-arn $assume_role_arn \
-       --role-session-name $assume_role_session_name)
-       
-  export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq -r .Credentials.AccessKeyId)
-  export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq -r .Credentials.SecretAccessKey)
-  export AWS_SESSION_TOKEN=$(echo $temp_role | jq -r .Credentials.SessionToken)
-  
-fi
-
-aws ecr get-login-password | docker login --username AWS --password-stdin $registry_url
+docker login --username AWS --password $password $registry_url
 docker push $registry_url/$image
--- push-ecr-image/main.tf
+++ push-ecr-image/main.tf
@@ -15,31 +15,8 @@ variable local_image {
   description = "The local image that should be tagged and pushed."
 }
 
-variable publisher_role_arn {
-  description = "The role that should be used for logging in to the docker ECR and publishing. If not set, the ambient credentials will be used."
-  default = ""
-}
+data aws_ecr_authorization_token token {
 
-data aws_caller_identity id {
-  
-}
-
-locals {
-  assume_role_args = length(var.publisher_role_arn) > 0 ? "${var.publisher_role_arn}" : ""
-}
-
-data external container_checksum {
-  program = ["${abspath(path.module)}/container-checksum.sh", var.local_image ]
-}
-
-
-resource null_resource image_push {
-  triggers = {
-    image_id = data.external.container_checksum.result.ImageID
-  }
-  provisioner local-exec {
-    command = "${abspath(path.module)}/push-ecr.sh ${var.local_image} ${var.registry_url} ${local.assume_role_args}"
-  }
 }
 
 data aws_caller_identity identity {
@@ -48,6 +25,19 @@ data aws_caller_identity identity {
 
 data aws_region current_region {
 
+}
+
+data external container_checksum {
+  program = ["${abspath(path.module)}/container-checksum.sh", var.local_image ]
+}
+
+resource null_resource image_push {
+  triggers = {
+    image_id = data.external.container_checksum.result.ImageID
+  }
+  provisioner local-exec {
+    command = "${abspath(path.module)}/push-ecr.sh ${var.local_image} ${var.registry_url} ${data.aws_ecr_authorization_token.token.password}"
+  }
 }
 
 output image_url {
